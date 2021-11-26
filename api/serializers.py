afb9913c40f6a22b43c76eb04e8c8f5615b0c747
izzat <izzat1998>
2021-11-26 15:03:08 +0500

    added extra checker for excel

--- api/serializers.py
+++ api/serializers.py
@@ -32,7 +32,7 @@ class ContainerInTerminalSerializerCreate(serializers.Serializer):
         date_of_arrived = validated_data.pop('date_of_arrived')
         staff = Staff.objects.filter(telegram_id=telegram_id)
         container, _ = Container.objects.get_or_create(name=container_name, weight_type=container_type)
-        if WaitingList.objects.filter(container=container).exists():
+        if WaitingList.objects.filter(container=container.id).exists():
             waiting_list_container = WaitingList.objects.get(container=container)
             waiting_list_container.delete()
         terminal = Terminal.objects.get(name=terminal_name)
