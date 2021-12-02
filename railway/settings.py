f832f3c8d90c85844ae8fd738770da420f6ee43d
izzat <izzat1998>
2021-12-02 17:55:52 +0500

    added crud system

--- railway/settings.py
+++ railway/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
-        'USER': 'izzat',
+        'USER': 'postgres',
         'HOST': 'localhost',
         'PASSWORD': '1',
         'PORT': '5432',
