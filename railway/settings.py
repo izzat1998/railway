4b476d4edefc5cf85a42ee58c61c8b32f2dbd212
izzat <izzat1998>
2021-12-09 12:12:51 +0500

    client side ready without proper permissions

--- railway/settings.py
+++ railway/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%&81bg0eu=^(c&^lg#_0euq(dg4=&2v003patwmfm^gpf$06z2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
